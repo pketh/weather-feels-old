ab7d4c58a1d57b23055bd81c20cf77af180b63ef
Pirijan ☃ <pirijan@gmail.com>
2016-04-02 22:56:04 -0400

    first commit

---
+++ Weather Cat/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  Weather Cat
+//
+//  Created by Pirijan on 2016-04-02.
+//  Copyright © 2016 Pirijan Ketheswaran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  //	@IBOutlet weak var window: NSWindow!
+
+  let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    if let button = statusItem.button {
+      button.image = NSImage(named: "StatusBarButtonImage")
+      let menu = NSMenu()
+      statusItem.menu = menu
+      menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
+      menu.addItem(NSMenuItem.separatorItem())
+      menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApp.terminate(_:)), keyEquivalent: "q"))
+      //func getLocation() {
+      // stub returns 'new york city/brooklyn'
+      // saves to global var
+      // }
+      // AppDelegate.getWeather(location) on the hour
+
+    }
+  }
+
+
+  // func getWeather(location) {
+  // gets current weather for *location
+  // }
+
+
+  func printQuote(sender: AnyObject) {
+    let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+    let quoteAuthor = "Mark Twain"
+    print("\(quoteText) — \(quoteAuthor)")
+  }
+
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+  
+  
+}
+
